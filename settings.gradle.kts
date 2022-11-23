060ef3f8c3df44a34f38d61dbca65b7acc3bde36
Benjamin Scholtes <88310985+bscholtes1A@users.noreply.github.com>
2022-11-23 14:40:56 +0100

    refactor(buid): apply the edc build plugin and the version catalog (#6)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,6 @@
-/*
- *  Copyright (c) 2022 Amadeus
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Amadeus - initial API and implementation
- *
- */
-
 rootProject.name = "trust-framework-adoption"
 
 // this is needed to have access to snapshot builds of plugins
-// that are used at the root project level, such as "module-names"
 pluginManagement {
     repositories {
         maven {
@@ -25,6 +10,34 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        mavenCentral()
+        mavenLocal()
+    }
+    versionCatalogs {
+        create("libs") {
+            from("org.eclipse.edc:edc-versions:0.0.1-SNAPSHOT")
+            version("jsonpath", "2.7.0")
+
+            library("jsonpath", "com.jayway.jsonpath", "json-path").versionRef("jsonpath")
+        }
+        create("edc") {
+            version("edc", "0.0.1-20221113-SNAPSHOT")
+            library("spi-core", "org.eclipse.edc", "core-spi").versionRef("edc")
+            library("spi-policy", "org.eclipse.edc", "policy-spi").versionRef("edc")
+            library("spi-policy-engine", "org.eclipse.edc", "policy-engine-spi").versionRef("edc")
+            library("policy-engine", "org.eclipse.edc", "policy-engine").versionRef("edc")
+            library("core-junit", "org.eclipse.edc", "junit").versionRef("edc")
+        }
+    }
+}
+
 
 include(":spi:common:trust-framework-spi")
 include(":extensions:common:trust-framework-policies")
